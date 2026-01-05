eb121dad430bc4529fecd1fda2aa3d88133fb29b
edounoqe <seohyun0456@gmail.com>
2026-01-05 17:38:33 +0900

    feat: CRUD 엔드포인트가진 todo 라우터 추가

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from model import Todo
+from fastapi import APIRouter, Path
+from model import Todo, TodoItem
 
 todo_router = APIRouter()
 
@@ -19,12 +19,24 @@ async def retrieve_todos() -> dict:
     }
 
 @todo_router.get("/todo/{todo_id}")
-async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
+async def get_single_todo(todo_id: int) -> dict:
     for todo in todo_list:
-        if todo.id== todo_id:
+        if todo.id == todo_id:
             return {
                 "todo": todo
             }
     return {
         "message": "Todo with supplied ID doesn't exist"
+    }
+
+@todo_router.put("/todo/{todo_id}")
+async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The ID of the todo to be updated")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            todo.item = todo_data.item
+            return {
+                "message": "Todo updated successfully"
+            }
+    return {
+        "message": "Todo with supplied ID doesnt exist"
     }
