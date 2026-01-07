52f866b29cf41bdb094430b3ab1a59e7980e102c
edounoqe <seohyun0456@gmail.com>
2026-01-07 15:35:04 +0900

    feat: 삭제 라우트 추가

--- todo.py
+++ todo.py
@@ -40,3 +40,23 @@ async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The I
     return {
         "message": "Todo with supplied ID doesnt exist"
     }
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_single_todo(todo_id: int) -> dict:
+    for index in range(len(todo_list)):
+        todo = todo_list[index]
+        if todo_id == todo_id:
+            todo_list.pop(index)
+        return {
+            "message": "Todo deleted successfully."
+        }
+    return {
+        "message": "Todo with supplied ID doesn't exist."
+    }
+
+@todo_router.delete("/todo")
+async def delete_all_todo() -> dict:
+    todo_list.clear()
+    return {
+        "message": "Todos deleted successfully"
+    }
